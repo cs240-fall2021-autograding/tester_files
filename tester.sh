43081c07b39565c5594c44bffab4751148a920b5
jgwak1 <jgwak1@binghamton.edu>
2021-10-16 13:26:05 -0400

    program interaction files

---
+++ tester.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+if [ $# -ne 2 ]; then
+	echo "bash tester.sh <executable> <testcase-number>"
+	exit 1
+fi
+
+
+# TESTS : avoid having space in the end of each test string as it can cause error in Expect.
+declare -A tests
+tests[1]=\
+$'Register user1\npassword1\nLogin user1\npassword1\nCreate\nactivity1\ncreator1\n111-233-222\n99-99-9999\n11-12/2000\n'
+
+# DESIRED RESULTS
+declare -A desired_result
+desired_result[1]="all 3 illegal dates rejected."
+
+
+# INTERACTION
+clear 
+interaction=$(expect tester.exp "$1" "${tests[$2]}" 2>&1)
+#key_results=$(echo "$interaction" | tail -n20)
+echo "$interaction"
+printf "\n*****desired_result*****\n\n${desired_result[$2]}\n\n"
+
+
+
+exit 0
